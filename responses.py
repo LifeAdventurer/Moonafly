d1b4f6512098ecfa4b20ba05bfcf65ccb511e2ca
lifeadventurer <life0adventurer@gmail.com>
2023-11-10 16:14:08 +0800

    update responses and prevent some eval() functions to be trigger

--- responses.py
+++ responses.py
@@ -10,15 +10,29 @@ def get_response(message) -> str:
 
   if msg[0] == '!':
     msg = msg[1:]
+    if msg[:4] == 'help':
+      return f'!math !gen'
+
     if msg[:4] == 'math':
-      msg = msg[4:]
+      msg = msg[5:]
+      if username == 'life_adventurer':
+        return eval(msg)
+      if 'shutdown' in msg or 'dir' in msg or 'c:\\' in msg:
+        return f'{username} 別搞事情阿 老哥'
+      elif 'os' in msg or 'local' in msg or 'global' in msg:
+        return f'{username} 想做什麼 別把我當 CTF 靶機'
       return eval(msg)
 
-    if msg[:3] == 'gen':
-      msg = msg[3:]
+    elif msg[:3] == 'gen':
+      msg = msg[4:]
       if 'vocabulary' in msg or 'vocab' in msg:
         return list[random.randint(0, len(list))]
+      if msg[7] == 'fortune':
+        return 'https://lifeadventurer.github.io/generators/fortune_generator/index.html'  
     
+    # elif msg[:6] == 'search':
+    #   msg = msg[7:]
+    #   if msg[:]
   
   if '睡覺' in msg:
     return f'{username} 晚安 祝好夢'
@@ -26,10 +40,10 @@ def get_response(message) -> str:
   if '機率' in msg:
     return f'{random.randint(1, 100)}%'
 
-  if msg == 'hello':
-    return 'hello'
-  
-  if '好' in msg:
+  if '危險' in msg:
+    return f'發生危險了嗎 需要幫您撥打119嗎'
+
+  if '好' in msg and '不好' not in msg:
     return '那麼好'
   
   if msg == '笑死':
