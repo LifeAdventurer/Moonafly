9ae3bccfac2b17ed0aced5025a8e627a21428faf
lifeadventurer <life0adventurer@gmail.com>
2023-11-11 13:07:35 +0800

    fix: !search command bug

--- responses.py
+++ responses.py
@@ -3,6 +3,7 @@ import json
 import random
 import requests
 from search_dict import search_dict
+import os
 
 vocab_file = open('vocabulary_items.json')
 list = json.load(vocab_file)['vocabularies']
@@ -16,28 +17,16 @@ def get_response(message) -> str:
   if msg[0] == '!':
     msg = msg[1:]
     if msg[:4] == 'help':
-      return "!math\n!gen\n!search"
+      return "!math\n!gen\n!search\n!dict"
 
     if msg[:4] == 'math':
       msg = msg[5:]
       return '此功能暫時關閉'
-      if username == 'life_adventurer':
-        return eval(msg)
-      elif username == 'tobiichi3227':
-        return '您數學那麼電 這裡沒有你需要的指令'
-
-      if 'shutdown' in msg or 'dir' in msg or 'c:\\' in msg or 'reverse' in msg or 'shell' in msg:
-        return f"{username} 別搞事情阿 老哥"
-
-      elif 'os' in msg or 'local' in msg or 'global' in msg:
-        return f"{username} 想做什麼 別把我當 CTF 靶機"
-
-      return eval(msg)
 
     elif msg[:3] == 'gen':
       msg = msg[4:]
       if msg[:4] == 'help':
-        return "vocabulary or (vocab) in instead\nfortune"
+        return "vocabulary or (vocab) instead\nfortune"
 
       elif 'vocabulary' in msg or 'vocab' in msg:
 
@@ -52,7 +41,9 @@ def get_response(message) -> str:
     elif msg[:6] == 'search':
       msg = msg[7:]
       if msg[:4] == 'help':
-        return 
+        return "git\ngithub"
+
+      
 
       elif msg[:6] == 'github':
         msg = user_message[15:]
@@ -62,6 +53,21 @@ def get_response(message) -> str:
           return f"The url {github_url} is not found (404 Not Found)."
         else:
           return github_url
+          
+      elif msg[:3] == 'git':
+        msg = msg[4:]
+        if msg[:4] == 'help':
+          return "setup -1\ninit -2\nstage & snapshot -3\nbranch & merge -4\ninspect & compare -5\nshare & update -6\n"
+        
+        msg = msg[1:]
+        if msg > '6' or msg < '1':
+          return 'no such command'
+
+        else:
+          return 'sorry this function is still developing'
+        # TO-DO
+        # elif msg == 'setup':
+
       else:
         return 'no such command'
 
@@ -75,8 +81,11 @@ def get_response(message) -> str:
   if '睡覺' in msg:
     return f"{username} 晚安 祝好夢"
 
+  if '晚安' in msg:
+    return msg
+
   if '機率' in msg:
-    return f"{random.randint(1, 100)}%"
+    return f"{((random.randint(1, len(msg)) ^ len(msg)) << len(msg)) % 100}%"
 
   if '危險' in msg:
     return '發生危險了嗎 需要幫您撥打119嗎'
