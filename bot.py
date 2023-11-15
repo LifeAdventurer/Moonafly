d7c71e1410bd2fa222650dfe8458c8ab114dc2a5
lifeadventurer <life0adventurer@gmail.com>
2023-11-16 01:32:11 +0800

    refactor bot.py and add some comments

--- bot.py
+++ bot.py
@@ -1,21 +1,21 @@
-import json
 import discord
 import responses
+import json
 
 token = ""
-# bot = discord.Bot()
 
-# Open Token JSON file
+# open token JSON file
 def load_token():
   global token
   token_file = open('config.json')
   token = json.load(token_file)['token']
   token_file.close()
 
-async def send_message(message, user_message, is_private):
+async def send_message(message, user_message):
   try:
+    # get response from responses.py
     response = responses.get_response(message)
-    await message.author.send(response) if is_private else await message.channel.send(response)
+    await message.channel.send(response)
   except Exception as e:
     print(e)
 
@@ -28,7 +28,7 @@ def run_discord_bot():
 
   @client.event
   async def on_ready():
-    print(f'{client.user} is now running!')
+    print(f'Moonafly is now running!')
 
   @client.event
   async def on_message(message):
@@ -38,16 +38,14 @@ def run_discord_bot():
     username = str(message.author)
     user_message = str(message.content)
     channel = str(message.channel)
-  
+
+    # avoid endless loops
     if not user_message:
       return
-      
-    print(f"{username} said: '{user_message}' ({channel})")
+    
+    # uncomment this line only for debug
+    # print(f"user: {username} \nmessage: '{user_message}'\nchannel: {channel}")
 
-    if user_message[0] == '?':
-      user_message = user_message[1:]
-      await send_message(message, user_message, is_private = True)
-    else:
-      await send_message(message, user_message, is_private = False)
+    await send_message(message, user_message)
 
   client.run(token)
