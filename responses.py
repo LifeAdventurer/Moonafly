084dad5d73a88f337d416bae9ddf592f2d7f0b0d
lifeadventurer <life0adventurer@gmail.com>
2023-11-19 21:43:26 +0800

    add password for terminal mode

--- responses.py
+++ responses.py
@@ -1,19 +1,52 @@
 import response_public
 import response_terminal
+import json
 
 is_public_mode = True
+entering_password = False
+incorrect_count = 0
+password = ""
+
+def load_password_for_terminal():
+  global password
+  passwords_file = open('passwords.json')
+  password = json.load(passwords_file)['terminal_password']
+  passwords_file.close()
 
 def get_response(message) -> str:
   username = str(message.author)
   msg = str(message.content)
 
-  global is_public_mode
+  global is_public_mode, entering_password, incorrect_count, password
+
+  if entering_password and incorrect_count < 3 and username != 'life_adventurer':
+    if msg == password:
+      password = password[::-1]
+      incorrect_count = 0
+      entering_password = False
+      is_public_mode = False
+      response_terminal.path_stack.append("~")
+      print('swap to terminal mode')
+      print('Moonafly:~$')
+      return '```Moonafly:~$```'
+    else:
+      incorrect_count += 1
+      if incorrect_count == 3:
+        incorrect_count = 0
+        entering_password = False
+        is_public_mode = True
+        return '```the maximum number of entries has been reached\nauto exited```'
+      return '```incorrect, please enter again```'
 
   if not is_public_mode and (msg == 'moonafly -p' or msg == 'Moonafly -p'):
     is_public_mode = True
     print('swap to public mode')
     return 'Successfully swap to public mode!'
   elif msg == 'moonafly -t' or msg == 'Moonafly -t':
+    if username != 'life_adventurer':
+      entering_password = True
+      load_password_for_terminal()
+      return '```please enter password```'
     is_public_mode = False
     response_terminal.path_stack.append("~")
     print('swap to terminal mode')
@@ -23,13 +56,14 @@ def get_response(message) -> str:
   else:
     if msg == 'exit' and not is_public_mode:
       is_public_mode = True
+      incorrect_count = 0
       response_terminal.path_stack.clear()
       return
     elif msg == 'status':
       if is_public_mode:
-        return 'public mode v1.0.0'
+        return 'public mode v1.0.1'
       else:
-        return f'terminal mode v1.0.3\n```{response_terminal.get_current_directory()}```'
+        return f'terminal mode v1.0.4\n```{response_terminal.get_current_directory()}```'
     if is_public_mode:
       return response_public.get_response_in_public_mode(message)
     else:
