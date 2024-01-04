5d30ce4e660678d5c1a622ef85be730be24477db
lifeadventurer <life0adventurer@gmail.com>
2024-01-05 00:33:11 +0800

    feat: make sure ~/random/vocab/test won't generate two identical words in a row
    and refactor a bit

--- src/cmd/random_vocab_test.py
+++ src/cmd/random_vocab_test.py
@@ -3,8 +3,11 @@ import json
 import random
 import terminal_mode
 
+
 random_vocab_testing = False
 vocab_index = 0
+previous_index = -1
+
 
 def get_random_vocab_test(message) -> str:
     username = str(message.author)
@@ -14,10 +17,11 @@ def get_random_vocab_test(message) -> str:
     # remove the leading and trailing spaces
     msg = msg.strip()
 
-    global random_vocab_testing, vocab_index
+    global random_vocab_testing, vocab_index, previous_index
 
     if msg.lower() == 'g':
         global vocabulary_list
+
         with open('../data/json/vocabulary_items.json', 'r', encoding='utf-8') as file:
             vocabulary_list = json.load(file)
         
@@ -31,7 +35,14 @@ def get_random_vocab_test(message) -> str:
                 """)
             else:
                 random_vocab_testing = True
-                vocab_index = random.randint(0, len(vocabulary_list[username]) - 1)
+                
+                random_index = random.randint(0, len(vocabulary_list[username]) - 1)
+                while random_index == previous_index:
+                    random_index = random.randint(0, len(vocabulary_list[username]) - 1)
+
+                previous_index = vocab_index
+                vocab_index = random_index
+
                 return textwrap.dedent(f"""
                     ```
                     {vocabulary_list[username][vocab_index]['word']}
@@ -51,13 +62,14 @@ def get_random_vocab_test(message) -> str:
     elif random_vocab_testing:
         if msg in vocabulary_list[username][vocab_index]['word_in_zh_TW']:
             random_vocab_testing = False
+
             vocabulary_list[username][vocab_index]['count'] -= 1
             word = vocabulary_list[username][vocab_index]['word']
             word_in_zh_TW = vocabulary_list[username][vocab_index]['word_in_zh_TW']
+
             if vocabulary_list[username][vocab_index]['count'] == 0:
                 del vocabulary_list[username][vocab_index]
-            with open('../data/json/vocabulary_items.json', 'w', encoding='utf-8') as file:
-                json.dump(vocabulary_list, file, indent=4, ensure_ascii=False)
+
             return textwrap.dedent(f"""
                 ```
                 Correct!
@@ -70,16 +82,21 @@ def get_random_vocab_test(message) -> str:
         else:
             random_vocab_testing = False
             vocabulary_list[username][vocab_index]['count'] += 1
-            with open('../data/json/vocabulary_items.json', 'w', encoding='utf-8') as file:
-                json.dump(vocabulary_list, file, indent=4, ensure_ascii=False)
+            word = vocabulary_list[username][vocab_index]['word']
+            word_in_zh_TW = vocabulary_list[username][vocab_index]['word_in_zh_TW']
+
             return textwrap.dedent(f"""
                 ```
-                Oops! Wrong
-                {vocabulary_list[username][vocab_index]['word']}
-                {vocabulary_list[username][vocab_index]['word_in_zh_TW']}
+                Oops! Wrong answer
+                {word}
+                {word_in_zh_TW}
                 {terminal_mode.current_path()}
                 ```
             """)
+
+        # update data
+        with open('../data/json/vocabulary_items.json', 'w', encoding='utf-8') as file:
+            json.dump(vocabulary_list, file, indent=4, ensure_ascii=False)
 
     else:
         return command_not_found(msg)
