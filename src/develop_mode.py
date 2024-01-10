a926b228f6bcd3100f4193881b357ac550b34aa8
lifeadventurer <life0adventurer@gmail.com>
2024-01-11 01:17:04 +0800

    feat: add cd command to develop mode

--- src/develop_mode.py
+++ src/develop_mode.py
@@ -56,6 +56,8 @@ def get_response_in_develop_mode(message) -> str:
     # remove the leading and trailing spaces
     msg = msg.strip()
 
+    global path_stack
+
     if msg == 'help':
         return textwrap.dedent(f"""\
             ```
@@ -65,7 +67,7 @@ def get_response_in_develop_mode(message) -> str:
             - terminal mode 
             - develop mode (current)
 
-             remote [file]
+             cd [dir]
              tree [-M]
             ```
         """)
@@ -77,9 +79,98 @@ def get_response_in_develop_mode(message) -> str:
             return command_help.load_help_cmd_info('remote_file')
         
         return remote.load_remote_file(msg, 'developer', username)
-    
+
+    # cd command
+    if msg[:2] == 'cd':
+        msg = msg[2:].lstrip()
+        if msg[:6] == '--help':
+            return command_help.load_help_cmd_info('cd')
+
+        path = msg
+
+        # blank or ~ should go directly to ~
+        if path == '' or path == '~':
+            path_stack = ['~']
+            return f"```{current_path()}```"
+
+        # go to the root directory
+        if path[0] == '/' and username != responses.author:
+            return textwrap.dedent(f"""
+                ```
+                permission denied
+                * this command requires the highest authority
+                {current_path()}
+                ```
+            """)
+
+        # skip all the '\' and split the path into a folder list
+        path = path.replace('\\', '').split('/')
+
+        # using [:] to prevent temporary_path_stack and path_stack affecting each other
+        temporary_path_stack = path_stack[:]
+
+        for folder in path:
+            # if the folder is empty or . then nothing happens with the
+            if folder == '' or folder == '.':
+                continue
+
+            # move up one directory
+            elif folder == '..':
+                if len(temporary_path_stack) > 1:
+                    temporary_path_stack.pop()
+
+                elif temporary_path_stack[0] == '~':
+                    # reverse the message to original command by removing the escape character
+                    msg = msg.replace("\\'", "'").replace("\\\"", "\"")
+                    space = ' ' * 4 * 7
+                    # multi-line adjustment
+                    msg = '\n'.join(
+                        [
+                            space + line if index > 0 else line
+                            for index, line in enumerate(msg.split('\n'))
+                        ]
+                    )
+                    return textwrap.dedent(f"""
+                        ```
+                        Moonafly: cd: {msg}: No such file or directory
+                        {current_path()}
+                        ```
+                    """)
+
+            else:
+                temporary_path_stack.append(folder)
+
+        current_directory = develop_mode_directory_structure
+
+        for folder in temporary_path_stack:
+            if folder in list(current_directory):
+                current_directory = current_directory[folder]
+
+            else:
+                # reverse the message to original command by removing the escape character
+                msg = msg.replace("\\'", "'").replace("\\\"", "\"")
+                space = ' ' * 4 * 6
+                # multi-line adjustment
+                msg = '\n'.join(
+                    [
+                        space + line if index > 0 else line
+                        for index, line in enumerate(msg.split('\n'))
+                    ]
+                )
+                return textwrap.dedent(f"""
+                    ```
+                    Moonafly: cd: {msg}: No such file or directory
+                    {current_path()}
+                    ```
+                """)
+
+        path_stack = temporary_path_stack
+        return f"```{current_path()}```"
+
+    # tree command
     elif msg[:4] == 'tree':
         msg = msg[4:].lstrip()
+
         if msg[:6] == '--help':
             return command_help.load_help_cmd_info('tree')
 
