328e13ddbec05607cd879b1e4813c68f2d15c239
lifeadventurer <life0adventurer@gmail.com>
2024-01-13 22:49:34 +0800

    impr: cannot use end command when Moonafly is not under maintenance

--- src/cmd/maintenance.py
+++ src/cmd/maintenance.py
@@ -56,6 +56,14 @@ def end_maintenance(msg: str) -> str:
 
     if msg[:6] == '--help':
         return command_help.load_help_cmd_info('end')
+    
+    if bot.in_maintenance != True:
+        return textwrap.dedent(f"""
+            ```
+            Moonafly is not under maintenance
+            {develop_mode.current_path()}
+            ```
+        """)
 
     with open('../data/txt/init_files/maintenance.txt', 'w') as file:
         file.write('False\n' + str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')) + '\n' + responses.develop_mode_current_using_user)
