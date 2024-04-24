1df4cd5a79867adf9babcc7827fc29cdcab96fa0
lifeadventurer <life0adventurer@gmail.com>
2024-04-24 23:11:29 +0800

    Refactor: Sort commands under terminal/shortcut alphabetically

--- src/command/shortcut.py
+++ src/command/shortcut.py
@@ -79,18 +79,18 @@ def get_shortcut_response(msg: str) -> str:
     if msg.startswith(HELP_FLAG):
         return command_help.load_help_cmd_info('shortcut')
 
-    if msg.startswith('set'):
-        msg = msg[4:].strip()
-        if msg.startswith(HELP_FLAG):
-            return command_help.load_help_cmd_info('shortcut_set')
-
-        return set_shortcut(msg)
-
-    elif msg.startswith('list'):
+    if msg.startswith('list'):
         msg = msg[5:].strip()
         if msg.startswith(HELP_FLAG):
             return command_help.load_help_cmd_info('shortcut_list')
 
         return list_user_shortcuts()
+
+    elif msg.startswith('set'):
+        msg = msg[4:].strip()
+        if msg.startswith(HELP_FLAG):
+            return command_help.load_help_cmd_info('shortcut_set')
+
+        return set_shortcut(msg)
 
     return terminal_mode.command_not_found(msg)
