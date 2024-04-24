4e983cc7673e0b3d707872b9ddb59be4d24d1336
lifeadventurer <life0adventurer@gmail.com>
2024-04-24 22:50:16 +0800

    Feat: Implement 'list' command for terminal/shortcut
    list user shortcuts with index

--- src/command/shortcut.py
+++ src/command/shortcut.py
@@ -5,7 +5,7 @@ import textwrap
 import responses
 import terminal_mode
 from command import command_help
-from constants import HELP_FLAG
+from constants import HELP_FLAG, TAB_SIZE
 
 
 def load_user_shortcuts() -> dict:
@@ -51,6 +51,32 @@ def set_shortcut(msg: str) -> str:
         return terminal_mode.handle_command_error('set', 'format')
 
 
+def list_user_shortcuts() -> str:
+    user_shortcuts = load_user_shortcuts()
+    username = responses.terminal_mode_current_using_user
+    shortcuts_count_strlen = len(
+        str(len(user_shortcuts.setdefault(username, {})))
+    )
+    shortcuts = []
+    for index, (key, value) in enumerate(
+        user_shortcuts[username].items(), start=1
+    ):
+        shortcuts.append(
+            f"{str(index).ljust(shortcuts_count_strlen)} \"{key}\" -> \"{value}\""
+        )
+
+    space = '\n' + ' ' * TAB_SIZE * 2
+    shortcuts = space.join(shortcuts)
+    return textwrap.dedent(
+        f"""
+        ```
+        {shortcuts}
+        {terminal_mode.current_path()}
+        ```
+        """
+    )
+
+
 def get_shortcut_response(msg: str) -> str:
     if msg.startswith(HELP_FLAG):
         return command_help.load_help_cmd_info('shortcut')
@@ -61,5 +87,12 @@ def get_shortcut_response(msg: str) -> str:
             return command_help.load_help_cmd_info('shortcut_set')
 
         return set_shortcut(msg)
+
+    elif msg.startswith('list'):
+        msg = msg[5:].strip()
+        if msg.startswith(HELP_FLAG):
+            return command_help.load_help_cmd_info('shortcut_list')
+
+        return list_user_shortcuts()
 
     return terminal_mode.command_not_found(msg)
