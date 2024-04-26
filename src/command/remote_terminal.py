9453af4add33f57e9746403a56d3acaa377c3363
lifeadventurer <life0adventurer@gmail.com>
2024-04-27 00:38:49 +0800

    Feat: Add support for code block syntax highlighting in ~/author/remote/terminal

--- src/command/remote_terminal.py
+++ src/command/remote_terminal.py
@@ -6,6 +6,13 @@ from command import command_help
 from constants import HELP_FLAG, TAB_SIZE
 
 
+def get_syntax_highlighting_language_code(terminal_output: str) -> str:
+    if all(token in terminal_output for token in ['---', '+++', '@@']):
+        return 'patch'
+
+    return 'markdown'
+
+
 def get_remote_terminal_response(msg: str) -> str:
     if msg.startswith(HELP_FLAG):
         return command_help.load_help_cmd_info('remote_terminal')
@@ -15,21 +22,23 @@ def get_remote_terminal_response(msg: str) -> str:
         result = subprocess.check_output(
             msg, shell=True, stderr=subprocess.STDOUT, universal_newlines=True
         )
+        language_code = get_syntax_highlighting_language_code(result)
         result = space.join(result.splitlines())
         return textwrap.dedent(
             f"""
-            ```
+            ```{language_code}
             {result}
             {terminal_mode.current_path()}
             ```
             """
         )
     except subprocess.CalledProcessError as e:
+        space += '- '
         error = space.join(e.output.splitlines())
         return textwrap.dedent(
             f"""
-            ```
-            error: {error}
+            ```diff
+            - {error}
             {terminal_mode.current_path()}
             ```
             """
