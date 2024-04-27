03778940fff9125829991269fd092b5129dcffb4
lifeadventurer <life0adventurer@gmail.com>
2024-04-27 14:40:13 +0800

    Fix: Preserve original indentations in help message when reading from txt files

--- src/command/command_help.py
+++ src/command/command_help.py
@@ -10,26 +10,22 @@ def load_help_cmd_info(command: str) -> str:
     space = ' ' * TAB_SIZE * 2
     path_to_txt = f"../data/txt/help_cmd_info/{command}.txt"
 
-    with open(path_to_txt, 'r') as information_file:
-        lines = information_file.readlines()
+    with open(path_to_txt, 'r') as file:
+        lines = file.readlines()
 
-    information = '\n'.join(
-        [
-            space + line.strip() if index > 0 else line.strip()
-            for index, line in enumerate(lines)
-        ]
-    )
+    information = space.join(lines)
 
     current_path = ''
     if responses.is_terminal_mode == True:
         current_path = terminal_mode.current_path()
     elif responses.is_develop_mode == True:
         current_path = develop_mode.current_path()
-
     return textwrap.dedent(
         f"""
         ```
         {information}
+        ```
+        ```
         {current_path}
         ```
         """
